cd6fb066ffc5613c1859f094e3c558ea7dd3d8c2
Qasem-moh <qasemcoder2020@gmail.com>
2024-10-26 22:23:42 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
+import SignUp from "./Components/SignUp";
 
-// import './App.css';
-import About from './Components/About';
-import Form from './Components/Form';
-import { data } from './data';
 function App() {
-
-  const showData = data.map((el) =>
-    <About
-      key={el.id}
-      name={el.name}
-      image={el.image}
-      price={el.price}
-      review={el.review}
-      descraption={el.description}
-    />)
   return (
-    <div className="App" 
-    style={{
-      marginTop:"20px",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      gap:"20px",
-      flexWrap:"wrap"
-    }}>
-      {/* {showData} */}
-<Form/>
+    <div className="App" >
 
+      <SignUp />
     </div>
   );
 }
---
+++ src/Components/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SignUp() {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Name..."/>
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;
