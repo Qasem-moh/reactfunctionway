9826fef3ccd662e93924a95e27fcffb0d51b68c5
Qasem-moh <qasemcoder2020@gmail.com>
2024-10-27 22:06:00 +0300

    react

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,13 +1,34 @@
 import React, { useState } from "react";
 import "../App.css";
+import axios from "axios";
 function SignUp() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordR, setPasswordR] = useState("");
+  const [accept,setAccept]=useState(false)
+  // const [flag,setFlag]=useState(false)
+  function submit(e){
+    let flag=true
+    e.preventDefault();
+    setAccept(true)
+    if(name===""||password.length<8||passwordR!==password){
+        flag=false
+    }else flag=true
+    if(flag){
+        //send data
+     let res= axios.post('http://127.0.0.1:8000/api/register',{
+            name:name,
+            email:email,
+            password:password,
+            password_confirmation:passwordR
+        }).then((t)=>console.log(t))
+        
+    }
+  }
   return (
     <div className="father">
-      <form>
+      <form onSubmit={submit}>
         <label htmlFor="userName">User Name: </label>
         <input
           type="text"
@@ -35,7 +56,7 @@ function SignUp() {
           required
           onChange={(e) => setPassword(e.target.value)}
         />
-
+{password.length<8&&accept&&(<p>password most be more than 8 charcters</p>)}
         <label htmlFor="repeatPassword"> repeat Password: </label>
         <input
           type="password"
@@ -44,6 +65,7 @@ function SignUp() {
           value={passwordR}
           onChange={(e) => setPasswordR(e.target.value)}
         />
+        {passwordR!==password&&accept&&(<p>password dose not match</p>)}
         <div>
           <button type="submit">Register</button>
         </div>
